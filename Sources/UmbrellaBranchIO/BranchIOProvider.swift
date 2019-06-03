51fcdd8376b4f6b9f78379e2a330d2b33017b945
Miguel Santiago <msantiago@beclocked.com>
2019-06-03 17:13:59 +0200

    importing branch always

--- Sources/UmbrellaBranchIO/BranchIOProvider.swift
+++ Sources/UmbrellaBranchIO/BranchIOProvider.swift
@@ -1,14 +1,16 @@
 import Foundation
+import Branch
 
 #if !COCOAPODS
 import Umbrella
-import Branch
 #endif
 
 final class BranchIOProvider: ProviderType {
   func log(_ eventName: String, parameters: [String: Any]?) {
     let event = BranchEvent.customEvent(withName: eventName)
-//    event.customData = parameters as! NSMutableDictionary
+    if let parameters = parameters as? NSMutableDictionary {
+        event.customData = parameters
+    }
 	event.logEvent()
   }
 }
