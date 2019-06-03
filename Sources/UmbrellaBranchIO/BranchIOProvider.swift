0ae2b0b3df006279c0bbad8edb4a16815c0e04de
Miguel Santiago <msantiago@beclocked.com>
2019-06-03 17:32:58 +0200

    public method

--- Sources/UmbrellaBranchIO/BranchIOProvider.swift
+++ Sources/UmbrellaBranchIO/BranchIOProvider.swift
@@ -6,11 +6,11 @@ import Umbrella
 #endif
 
 open class BranchIOProvider: ProviderType {
-  func log(_ eventName: String, parameters: [String: Any]?) {
-    let event = BranchEvent.customEvent(withName: eventName)
-    if let parameters = parameters as? NSMutableDictionary {
-        event.customData = parameters
+    public func log(_ eventName: String, parameters: [String: Any]?) {
+        let event = BranchEvent.customEvent(withName: eventName)
+        if let parameters = parameters as? NSMutableDictionary {
+            event.customData = parameters
+        }
+        event.logEvent()
     }
-	event.logEvent()
-  }
 }
